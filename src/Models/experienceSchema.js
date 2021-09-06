72188e5fb6ebbd780b3640619525eb584f205a1e
attila <attilavigh.contact@gmail.com>
2021-09-06 10:23:58 +0100

    Experiences basic CRUD functions done and tested.

--- src/Models/experienceSchema.js
+++ src/Models/experienceSchema.js
@@ -2,23 +2,20 @@ import mongoose from 'mongoose';
 
 const { Schema, model } = mongoose;
 
-const reviewsSchema = new Schema(
+const experienceSchema = new Schema(
     {
-        // "_id": "5d925e677360c41e0046d1f5",  //server generated
-        // "role": "CTO",
-        // "company": "Strive School",
-        // "startDate": "2019-06-16T22:00:00.000Z",
-        // "endDate": "2019-06-16T22:00:00.000Z", //could be null
-        // "description": "Doing stuff here and there",
-        // "area": "Berlin",
-        // "username": "admin",
-        // "createdAt": "2019-09-30T19:58:31.019Z",  //server generated
-        // "updatedAt": "2019-09-30T19:58:31.019Z",  //server generated
-        // "image": ... //server generated on upload, set a default here
+        username   : { type: String, required: true},   //"admin",
+        role       : { type: String, required: true},   //"CTO",
+        company    : { type: String, required: true},   //"Strive School",
+        startDate  : { type: Date  , required: true},   //"2019-06-16T22:00:00.000Z",
+        endDate    : { type: Date  , required: true},   //"2019-06-16T22:00:00.000Z", //could be null
+        description: { type: String, required: true},   //"Doing stuff here and there",
+        area       : { type: String, required: true},   //"Berlin",
+        image      : { type: String, required: true},   //... //server generated on upload, set a default here
     },
     {
         timestamps: true,
     }
 );
 
-export default model('Review', reviewsSchema);
+export default model('Experience', experienceSchema);
--- src/services/experience/handler.js
+++ src/services/experience/handler.js
@@ -1,118 +1,81 @@
 import createError from "http-errors";
 import Experience from "../../Models/experienceSchema.js";
 
-export const getAllExperience = async ( req, res, next ) => {
-    try {
-        const experienceList = await Experience.find( {} );
-        if ( req.query && req.query.name )
-        {
-            const filteredExperience = experienceList.filter( author =>
-                author.name
-                    .toLowerCase()
-                    .includes( req.query.name.toLowerCase() )
-            );
-            res.send( filteredExperience );
-        }
-        else
-            res.send( experienceList );
-    }
-    catch ( error ) {
-        next( createError( 404, error.message ) );
-    }
-};
-
 export const addExperience = async ( req, res, next ) => {
     console.log( "req.params.id::: ", req.body );
-    try {
+    try
+    {
         const newExperience = await new Experience( req.body ).save();
         res.status( 201 ).send( newExperience );
     }
-    catch ( error ) {
+    catch ( error )
+    {
         next( createError( 500, error.message ) );
     }
 };
 
-export const searchExperienceByTitle = async ({req, res, next}) => {
-    console.log("req.query ", req.query);
-    try {
-        const experienceTitle = await Experience.find(req.query.title);
-        res.send(experienceTitle);
+export const getAllExperience = async ( req, res, next ) => {
+    try
+    {
+
+        const experience = await Experience.find( {} );
+        res.send( experience );
+
     }
-    catch (error) {
-        next(createError(500, error.message));
+    catch ( error )
+    {
+        next( createError( 404, error.message ) );
     }
 };
 
-export const findById = async (req, res, next) => {
-    console.log("req.params.id::: ", req.params.id);
-    try {
-        const Experience = await Experience.findById(req.params.id);
-        if(!Experience) {
-            next(createError(404, `Experience with id "${ req.params.id }" not found`));}
-        res.send(Experience);
+export const findById = async ( req, res, next ) => {
+    console.log( "req.params.id::: ", req.params.id );
+    try
+    {
+        const experience = await Experience.findById( req.params.id );
+        if ( !experience )
+        {
+            next( createError( 404, `Experience with id "${ req.params.id }" not found` ) );
+        }
+        res.send( experience );
     }
-    catch (error) {
-        next(createError(500, error.message));
-    }
-};
-
-export const updateExperience = async (req, res, next) => {
-    try {
-        const updatedAuthor = await Experience.findByIdAndUpdate( req.params.id, req.body, { new: true } );
-        res.status(204).send(updatedAuthor);
-    }
-    catch (error) {
-        next(createError(500, error.message));
+    catch ( error )
+    {
+        next( createError( 500, error.message ) );
     }
 };
 
-export const deleteExperience = async (req, res, next) => {
-    try {
-        await Experience.findByIdAndDelete( req.params.id);
-        res.status(204).send();
+export const updateExperience = async ( req, res, next ) => {
+    try
+    {
+        const updatedExperience = await Experience.findByIdAndUpdate( req.params.id, req.body, { new: true } );
+        res.status( 204 ).send( updatedExperience );
     }
-    catch (error) {
-        next(createError(500, error.message));
+    catch ( error )
+    {
+        next( createError( 500, error.message ) );
     }
 };
 
-export const getExperienceCommentsById = async (req, res, next) => {
-    try {
-        const ExperienceComments = await Experience.findById(req.params.id);
-        if(!ExperienceComments) 
-            next(createError(404, `Experience with id "${ req.params.id }" not found`));
-        res.send(ExperienceComments);
-    }
-    catch (error) {
-        next(createError(500, error.message));
-    }
-}
 
-export const addComment = async (req, res, next) => {
-    try {
-        const updatedExperience = await Experience.findByIdAndUpdate( req.params.id, { $push: { comments: req.body } }, { new: true } );
-        res.status(204).send(updatedExperience);
+export const deleteExperience = async ( req, res, next ) => {
+    try
+    {
+        await Experience.findByIdAndDelete( req.params.id );
+        res.status( 204 ).send();
     }
-    catch (error) {
-        next(createError(500, error.message));
+    catch ( error )
+    {
+        next( createError( 500, error.message ) );
     }
-}
+};
 
-export const deleteComment = async (req, res, next) => {
-    try {
-        const updatedExperience = await Experience.findByIdAndDelete( req.params.id, { $pull: { _id: req.params.commentId } }, { new: true } );
-        res.status(204).send(updatedExperience);
-    }
-    catch (error) {
-        next(createError(500, error.message));
-    }
-}
-const cartHandler = {
+const experienceHandler = {
     getAll: getAllExperience,
     add: addExperience,
     findById: findById,
     update: updateExperience,
-    delete: deleteExperience,
+    delete: deleteExperience
 };
 
-export default cartHandler;
+export default experienceHandler;
