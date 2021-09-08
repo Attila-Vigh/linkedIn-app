ee7beb326c5c82f950d4177bf670e3d9ac0ce42c
pavan kumar <pavan.yalamanchi@gmail.com>
2021-09-08 19:37:04 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,16 +5,16 @@ import mongoose from 'mongoose';
 const { PORT, MONGO_CONNECTION_LOCAL, MONGO_CONNECTION_CLOUD, MONGO_CONNECTION_iCLOUD } = process.env;
 
 
-console.table( listEndpoints( server ) );
+console.table(listEndpoints(server));
 
 
-mongoose.connect( MONGO_CONNECTION_CLOUD );
+mongoose.connect(MONGO_CONNECTION_CLOUD);
 
-mongoose.connection.on( 'connected', () => {
-    console.info( `✅ Connected to MongoDB at ${ MONGO_CONNECTION_iCLOUD } 🤞🤞🚀🚀🚀` );
-    server.listen( PORT, () => console.log( `✅ Server listening on port 🥳 ${ PORT } 🥳 🥳🥳🥳🥳🥳 :` ) );
-    server.on( 'error', ( err ) => console.log( `❌ Server error: ${ err } 🥵🥵🥵🥵🥵🥵🥵  ` ) );
+mongoose.connection.on('connected', () => {
+    console.info(`✅ Connected to MongoDB at ${ MONGO_CONNECTION_CLOUD } 🤞🤞🚀🚀🚀`);
+    server.listen(PORT, () => console.log(`✅ Server listening on port 🥳 ${ PORT } 🥳 🥳🥳🥳🥳🥳 :`));
+    server.on('error', (err) => console.log(`❌ Server error: ${ err } 🥵🥵🥵🥵🥵🥵🥵  `));
 
-} );
+});
 
-mongoose.connection.on( 'error', ( err ) => console.log( `❌ Mongo error: ${ err } 📛📛📛📛📛📛📛📛  ` ) );
+mongoose.connection.on('error', (err) => console.log(`❌ Mongo error: ${ err } 📛📛📛📛📛📛📛📛  `));
